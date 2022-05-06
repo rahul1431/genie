07352eefa5ccbda5bf0372be36868c243b167909
sergey <sishkoser@outlook.com>
2022-05-06 10:03:09 +0800

    Firebase, Google MAP

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,7 +10,7 @@ import GoogleMaps
   ) -> Bool {
     var flutter_native_splash = 1
     UIApplication.shared.isStatusBarHidden = false
-    GMSServices.provideAPIKey("AIzaSyBP-FuQdSf7piRlI082kO3KWkFWas_QvaY")
+    GMSServices.provideAPIKey("AIzaSyCI5o-44drjlAQX4sTCjwzukteUuw9Wb2Y")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
